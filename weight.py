12befb099d0702a88564ec4977a2e096ae1a80f7
Yuya Imoto <imoto.yuya@icloud.com>
2018-05-12 13:51:38 +0900

    100個の中から重みをつけて10個選択する

--- weight.py
+++ weight.py
@@ -1,16 +1,26 @@
 import random
 
-itemSize = 10
-weight = [itemSize - i for i in range(itemSize)]
-weightSum = sum(weight)
-randomValue = random.randrange(weightSum)
-print(randomValue)
+def getIndex(weight):
+    weightSum = sum(weight)
+    randomValue = random.randrange(weightSum)
 
-sum = 0
-index = -1
-for w in weight:
-    sum += w
-    if randomValue < sum:
-        index = weight.index(w)
-        break
-print(index)
+    cumulativeSum = 0
+    index = -1
+    for w in weight:
+        cumulativeSum += w
+        if randomValue < cumulativeSum:
+            index = weight.index(w)
+            break
+    return index
+
+if __name__ == '__main__':
+    itemSize = 100
+    weight = [itemSize - i for i in range(itemSize)]
+    needNum = 10
+    indexList = []
+    while len(indexList) < needNum:
+        index = getIndex(weight)
+        if index not in indexList:
+            indexList.append(index)
+    indexList.sort()
+    print(indexList)
